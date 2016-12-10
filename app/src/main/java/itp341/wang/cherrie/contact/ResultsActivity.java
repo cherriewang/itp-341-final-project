05dea5b9590d537beacfb67c90b611a7c72beb51
cherriewang <cherrie.cherry@gmail.com>
2016-12-10 13:46:02 -0800

    HOUSTON WE HAVE DATA

--- app/src/main/java/itp341/wang/cherrie/contact/ResultsActivity.java
+++ app/src/main/java/itp341/wang/cherrie/contact/ResultsActivity.java
@@ -22,6 +22,7 @@ public class ResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        getSupportActionBar().hide();
 
         Intent intent = getIntent();
         HashMap<String, ArrayList<Senator>> repMap =
